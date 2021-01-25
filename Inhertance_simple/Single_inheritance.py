c742c746118aaf4a84ca431d26c1542d9eee004b
Trishala-Sin <trishla.singh35@gmail.com>
2021-01-25 14:40:54 +0530

    commit1

---
+++ Inhertance_simple/Single_inheritance.py
@@ -0,0 +1,35 @@
+# Single Inheritance : only 1 child
+
+# Base or Super class. Note object in bracket.
+# (Generally, object is made ancestor of all classes)
+# In Python 3.x "class Person" is
+# equivalent to "class Person(object)"
+
+
+class person:
+    # Constructor
+    def __init__(self,name):
+        self.name = name
+    # To get name
+    def getname(self):
+        return self.name
+    # To checkk if the person is employee
+    def isEmployee(self):
+        return False
+
+
+# Inherited or Subclass (Note Person in bracket)
+class Employee(Person):
+
+    # Here we return true
+    def isEmployee(self):
+        return True
+
+
+# Driver code
+emp = Person("Geek1")  # An Object of Person
+print(emp.getName(), emp.isEmployee())
+
+emp = Employee("Geek2")  # An Object of Employee
+print(emp.getName(), emp.isEmployee())
+
