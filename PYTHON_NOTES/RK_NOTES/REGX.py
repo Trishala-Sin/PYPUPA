07f8032d82b6dd6ce7b92543ce5c8e5e4b9413e4
Trishala singh <Trishla.singh35@gmail.com>
2021-08-18 17:07:04 +0530

    regx

--- PYTHON_NOTES/RK_NOTES/REGX.py
+++ PYTHON_NOTES/RK_NOTES/REGX.py
@@ -8,17 +8,20 @@ def replace_latest_slice(str):
     a = {}
     for i in re.finditer("latest_slice::", str):
         st = i.start()
-        f = str.find(' ', i.end() + 1)
+        f = str.find(" ", i.end() + 1)
         f = len(str) if f == -1 else f
-        tab = r[st:f].split()[-1]
+        tab = r[st:f].split('::')[-1]
+        print(tab)
         rep = "(SELECT * FROM " + tab + " WHERE DS = 'LATEST')"
 
         a[str[st:f]] = rep
 
     for key, val in a.items():
-        str = str.replace(key, val)
+        #print(key)
+        #print(val)
+       str = str.replace(key, val)
     return str
 
 
-print("orignal) :" + r)
-print("answer :" + replace_latest_slice(r))
+#print("orignal) :" + r)
+print( replace_latest_slice(r))
